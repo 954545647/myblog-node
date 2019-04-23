4b8b7fe1d7605e2396d6022efe6858d5cb58f06a
954545647 <954545647@qq.com>
2019-04-23 23:32:07 +0800

    init pro

---
+++ app.js
@@ -0,0 +1,27 @@
+const Koa = require("koa");
+const app = new Koa();
+const router = require('koa-router')();
+const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
+const user = require('./interface/user.js');
+const render = require('koa-art-template');
+const path = require('path');
+render(app, {
+  root: path.join(__dirname, 'views'),
+  extname: '.html',
+  debug: process.env.NODE_ENV !== 'production'
+});
+
+app.use(bodyParser());
+app.use(cors());
+
+router.get('/',async(ctx)=>{
+  ctx.body='这是首页';
+  await ctx.render('mail');
+})
+
+app.use(user.routes()).use(user.allowedMethods());  //用户路由
+app
+  .use(router.routes()) // 启动路由
+  .use(router.allowedMethods());
+app.listen(3000);
---
+++ src/mail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const fs  = require('fs');
+const path = require('path');
+let transporter = nodemailer.createTransport({
+  // host: 'smtp.ethereal.email',
+  service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+  // port: 465, // SMTP 端口
+  // secureConnection: true, // 使用了 SSL
+  auth: {
+    user: '954545647@qq.com',
+    // 这里密码不是qq密码，是你设置的smtp授权码
+    pass: 'cdixterherclbbfb',
+  }
+});
+
+let mailOptions = {
+  from: '"认证邮件" <954545647@qq.com>', // sender address
+  to: '954545647@qq.com', // list of receivers
+  subject: 'Hello', // Subject line
+  // 发送text或者html格式
+  // text: 'Hello world?', // plain text body
+  // html: '<b>Hello world?</b>' // html body
+  html: fs.createReadStream(path.resolve(__dirname, 'email.html')) 
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
+});
