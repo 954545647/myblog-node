091e6e1a8e18ce3c0f64dd9e87fc66a1c76615ae
954545647 <954545647@qq.com>
2019-06-07 22:41:46 +0800

    添加图表

--- interface/blog.js
+++ interface/blog.js
@@ -14,7 +14,7 @@ const bodyParser = require("koa-bodyparser");
 app.use(bodyParser());
 // 配置mongodb数据库
 const Blog = require("./../db/models/blog.js");
-
+const User = require("./../db/models/user.js");
 // const CONFIG = require("./../config/session.js"); // session的默认配置
 // var session = require("koa-session"); // 这个是帮助koa解析cookie
 // app.keys = ["some secret hurr"];
@@ -38,8 +38,6 @@ var upload = multer({
   storage: storage
 });
 
-
-
 // 上传图片配置
 router.post("/upload", upload.single("file"), async (ctx, next) => {
   let file = ctx.req.file;
@@ -53,9 +51,9 @@ router.get("/getAllBlog", async (ctx, next) => {
   // 从数据库中查找博客类型为1的,即非草稿
   let blog = await Blog.find({ state: 1 });
   let blogTitles = [];
-  blog.forEach((item)=>{
-    blogTitles.push(item.title)
-  })
+  blog.forEach(item => {
+    blogTitles.push(item.title);
+  });
   if (blog) {
     ctx.body = {
       status: 200,
@@ -68,11 +66,10 @@ router.get("/getAllBlog", async (ctx, next) => {
       status: 200,
       code: 1, //数据库为空
       blog: [],
-      blogTitles:[]
+      blogTitles: []
     };
   }
 });
-
 
 // 获取指定博客
 router.get("/getBlog", async (ctx, next) => {
@@ -115,6 +112,49 @@ router.post("/saveBlog", async (ctx, next) => {
   ctx.body = {
     status: 200,
     blog
+  };
+});
+
+// 获取系统信息 (博客数量(草稿和正文) 用户数量(超级管理员和普通用户))
+router.get("/info", async ctx => {
+  let userInfo = {};
+  let blogInfo = {};
+  await User.find().then(res => {
+    userInfo.count = res.length;
+    let count = 0;
+    res.forEach(item => {
+      if (item.role === 0) {
+        count++;
+      }
+    });
+    userInfo.superAdmin = res.length - count;
+    userInfo.ordinary = count;
+  });
+  await Blog.find().then(res => {
+    blogInfo.count = res.length;
+    let count = 0;
+    let blogTitles = [];
+    // 博客数据
+    res.forEach(item => {
+      if (item.state === 0) {
+        count++;
+      }
+    });
+    blogInfo.draft = count;
+    blogInfo.main = res.length - count;
+    // 博客标题
+    res.forEach((item)=>{
+      if(item.state === 1){
+        blogTitles.push(item.title)
+      }
+    })
+    blogInfo.blogTitles = blogTitles;
+  });
+  ctx.body = {
+    code: 0,
+    status: 200,
+    userInfo,
+    blogInfo
   };
 });
 
